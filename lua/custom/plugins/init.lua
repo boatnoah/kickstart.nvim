18ae7ded5959dcbd88010fde6439bcea35534b00
boatnoah <noahskim7@gmail.com>
2024-07-03 01:51:57 -0700

    plugin: harpoon added

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -30,57 +30,35 @@ return {
     config = function()
       local alpha = require 'alpha'
       local dashboard = require 'alpha.themes.dashboard'
-      dashboard.section.header.val = {
-        '      ',
-        '      ',
-        '      ',
-        '      ',
-        '      ',
-        '      ',
-        '      ',
-        '      ',
-        '  ⟋|､',
-        ' (°､ ｡ 7',
-        ' |､  ~ヽ',
-        ' じしf_,)〳',
+
+      -- Center the header (cat ASCII art)
+      local header = {
+        '          ⟋|､         ',
+        '         (°､ ｡ 7       ',
+        '         |､  ~ヽ       ',
+        '         じしf_,)〳    ',
       }
 
+      dashboard.section.header.val = header
+      dashboard.section.header.opts.hl = 'AlphaHeader'
+
+      -- Simplified button configuration
       dashboard.section.buttons.val = {
-        {
-          type = 'button',
-          val = 'Find Files',
-          on_press = function()
-            local key = vim.api.nvim_replace_termcodes('<leader>ff', true, false, true)
-            vim.api.nvim_feedkeys(key, 'normal', false)
-          end,
-          opts = {
-            position = 'center',
-            shortcut = 'SPC f f',
-            cursor = 5,
-            width = 22,
-            align_shortcut = 'right',
-            hl_shortcut = 'AlphaShortcut',
-          },
-        },
-        {
-          type = 'button',
-          val = 'Find Word',
-          on_press = function()
-            local key = vim.api.nvim_replace_termcodes('<leader>fw', true, false, true)
-            vim.api.nvim_feedkeys(key, 'normal', false)
-          end,
-          opts = {
-            position = 'center',
-            shortcut = 'SPC f w',
-            cursor = 5,
-            width = 22,
-            align_shortcut = 'right',
-            hl_shortcut = 'AlphaShortcut',
-          },
-        },
+        dashboard.button('f f', '  Find Files', ':Telescope find_files<CR>'),
+        dashboard.button('f w', '  Find Word', ':Telescope live_grep<CR>'),
       }
 
-      alpha.setup(dashboard.opts)
+      -- Set up the config
+      dashboard.config.layout = {
+        { type = 'padding', val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) } },
+        dashboard.section.header,
+        { type = 'padding', val = 5 },
+        dashboard.section.buttons,
+        { type = 'padding', val = 3 },
+        dashboard.section.footer,
+      }
+
+      alpha.setup(dashboard.config)
     end,
   },
   {
@@ -97,5 +75,38 @@ return {
   },
   {
     'ThePrimeagen/vim-be-good',
+  },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+
+      -- REQUIRED
+      harpoon:setup()
+
+      -- Set up keymaps
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set('n', '<leader>e', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      -- Use leader key combinations for navigation
+      vim.keymap.set('n', '<leader>1', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<leader>2', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<leader>3', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<leader>4', function()
+        harpoon:list():select(4)
+      end)
+    end,
   },
 }
